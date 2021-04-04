224510a772570ad43371a5157d22e17fb7c9cf19
Amir Shojae <amirshojae@alphafx.co.uk>
2021-04-04 01:15:31 +0100

    installed custom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import WWWButton from 'worldwideandweb-storybook/lib/Button/Button';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <WWWButton />
       </header>
     </div>
   );
