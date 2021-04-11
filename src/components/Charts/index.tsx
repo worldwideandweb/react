f3aa75ef825e511fc941d36018b7c44369289ed7
Amir Shojae <amirshojae@alphafx.co.uk>
2021-04-11 21:12:43 +0100

    make charts easier to follow

--- src/components/Charts/index.tsx
+++ src/components/Charts/index.tsx
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import WWWCard from 'worldwideandweb-storybook/dist/components/Card';
+import theme from 'worldwideandweb-storybook/dist/theme/theme';
 
 interface IProps {
   children: JSX.Element | JSX.Element[];
@@ -16,8 +17,11 @@ const useStyles = makeStyles({
     boxSizing: 'border-box',
     transition: 'all .2s ease-in-out',
     '&:hover': {
-      transform: 'scale(1.02)',
+      transform: 'scale(1.1)',
     },
+    [theme.breakpoints.down('sm')]: {
+        padding: '8px',
+      },  
   },
 });
 
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Bar, Line, Radar } from 'react-chartjs-2';
 import {
   Grid,
-  Typography
+  Typography,
 } from 'worldwideandweb-storybook/dist/components/External';
 import theme from 'worldwideandweb-storybook/dist/theme/theme';
 import Chart from '../Charts';
